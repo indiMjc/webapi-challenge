40540b9fc0cd02987ed23caa21dae17444cc89db
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-06 12:07:38 -0500

    post new action by project id endpoint

---
+++ projectActions/projectActionRouter.js
@@ -0,0 +1,42 @@
+const express = require("express");
+
+const router = express.Router();
+
+const ActionsDb = require("../data/helpers/actionModel");
+const ProjectsDb = require("../data/helpers/projectModel");
+
+router.use(express.json());
+
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  ActionsDb.get(id)
+    .then(actions => {
+      res.status(200).json(actions);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "Internal error while getting project actions." });
+    });
+});
+
+router.post("/:id", (req, res) => {
+  const newAction = {
+    project_id: req.params.id,
+    description: req.body.description,
+    notes: req.body.notes
+  };
+  ActionsDb.insert(newAction)
+    .then(action => {
+      res.status(201).json(action);
+    })
+    .catch(err => {
+      console.log(err);
+      res
+        .status(500)
+        .json({ error: "Internal error while saving new project action." });
+    });
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -3,9 +3,11 @@ const express = require("express");
 const server = express();
 
 const projectRouter = require("./projects/projectRouter");
+const actionsRouter = require("./projectActions/projectActionRouter");
 
 server.use(express.json());
 server.use("/projects", projectRouter);
+server.use("/actions", actionsRouter);
 
 server.get("/", (req, res) => {
   const msg = "Server Running!";
