fe60378d0704481dfd65242e1ae1fd121b768c1c
indiMjc <45151346+indiMjc@users.noreply.github.com>
2019-12-06 11:33:26 -0500

    get project by id

--- projects/projectRouter.js
+++ projects/projectRouter.js
@@ -17,4 +17,16 @@ router.get("/", (req, res) => {
     });
 });
 
+router.get("/:id", (req, res) => {
+  const { id } = req.params;
+  ProjectDb.getById(id)
+    .then(project => {
+      res.status(200).json(project);
+    })
+    .catch(err => {
+      console.log(err);
+      res.status(500).json({ error: "Internal error while getting project." });
+    });
+});
+
 module.exports = router;
